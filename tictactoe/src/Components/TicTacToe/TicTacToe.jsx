c5781b719541dfdeb4f3195fa3e1c611b2f9d2ec
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-03 18:19:20 +0530

    Update TicTacToe.jsx

--- tictactoe/src/Components/TicTacToe/TicTacToe.jsx
+++ tictactoe/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTacToe.css'
 import circle from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
 
+let data = ["","","","","","","","",""];
+
 function TicTacToe() {
+
+    let [count, setCount] = useState(0);
+    let [lock, setLock] = useState(false);
+
+    const toggle = (w, num) => {
+        if (lock) {
+            return 0;
+        }
+        if (count % 2 === 0) {
+            e.target.innerHTML = `<img src = '${cross_icon}'>`;
+            data[num] = "x";
+            setCount(++count);
+        } else {
+            e.target.innerHTML = `<img src = '${circle}'>`;
+            data[num] = "o";
+            setCount(++count);
+        }
+
+    }
+
   return (
     <div className='container'>
         <h1 className="title">TicTacToe Game in <span>React</span></h1>
