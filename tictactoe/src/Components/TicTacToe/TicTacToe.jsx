decdca28563ce03c138f8a99e01f80bba2c44937
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-03 18:05:43 +0530

    Update TicTacToe.jsx

--- tictactoe/src/Components/TicTacToe/TicTacToe.jsx
+++ tictactoe/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './TicTacToe.css'
+import circle from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
 
 function TicTacToe() {
   return (
-    <div>TicTacToe</div>
+    <div className='container'>
+        <h1 className="title">TicTacToe Game in <span>React</span></h1>
+        <div className="board">
+
+        </div>
+        <button className="reset">Reset</button>
+    </div>
   )
 }
 
