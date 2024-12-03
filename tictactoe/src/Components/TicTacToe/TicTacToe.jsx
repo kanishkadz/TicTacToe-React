cb33dc07450bf9ab09dcc31ede47a8d02684be4e
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-03 18:34:10 +0530

    Update TicTacToe.jsx

--- tictactoe/src/Components/TicTacToe/TicTacToe.jsx
+++ tictactoe/src/Components/TicTacToe/TicTacToe.jsx
@@ -33,6 +33,18 @@ function TicTacToe() {
             won(data);
         } else if(data[6] === data[7] && data[7] === data[8] && data[8]!=="") {
             won(data);
+        } else if(data[0] === data[3] && data[3] === data[6] && data[6]!=="") {
+            won(data);
+        } else if(data[1] === data[4] && data[4] === data[7] && data[7]!=="") {
+            won(data);
+        } else if(data[2] === data[5] && data[5] === data[8] && data[8]!=="") {
+            won(data);
+        } else if(data[0] === data[4] && data[4] === data[8] && data[8]!=="") {
+            won(data);
+        } else if(data[0] === data[1] && data[1] === data[2] && data[2]!=="") {
+            won(data);
+        } else if(data[2] === data[4] && data[4] === data[6] && data[6]!=="") {
+            won(data);
         }
     }
 
